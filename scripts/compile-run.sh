7c0cd8b1a1b328aa2ef64e27c6990bcd36cc4a83
juliuswasajja <65942820+juliuswasajja@users.noreply.github.com>
2024-08-07 12:09:13 +0200

    user work-floe process attended to

---
+++ scripts/compile-run.sh
@@ -0,0 +1,11 @@
+# javac ../src/App.java
+# javac ../src/Menu.java
+
+# javac ../src/User.java
+# javac ../src/Admin.java
+# javac ../src/Patient.java
+# javac ../src/Login.java
+
+# javac ../src/App.java
+
+# java ../src/App
